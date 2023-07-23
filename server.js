764742907f751a5723dbef0f9eb36752f1dcdb8f
Giora Marani <giora.marani@gmail.com>
2023-07-23 16:07:06 +0300

    add als

--- server.js
+++ server.js
@@ -40,10 +40,16 @@ if (process.env.NODE_ENV === 'production') {
 import { authRoutes } from './api/auth/auth.routes.js'
 import { userRoutes } from './api/user/user.routes.js'
 import { toyRoutes } from './api/toy/toy.routes.js'
+import { reviewRoutes } from './api/review/review.routes.js'
+
 
 // routes
+import { setupAsyncLocalStorage } from './middlewares/setupAls.middleware.js'
+app.all('*', setupAsyncLocalStorage)
+
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
+app.use('/api/review', reviewRoutes)
 app.use('/api/toy', toyRoutes)
 
 // Make every unmatched server-side-route fall back to index.html
