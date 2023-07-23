37173f72fcc52c31c4c307d684c75131cfe3df42
Giora Marani <giora.marani@gmail.com>
2023-07-23 16:50:06 +0300

    add .env

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,14 @@
+// export default {
+//     dbURL: 'mongodb+srv://gioramarani:1234@cluster0.pdpj63k.mongodb.net/',
+//     dbName: 'toyDB',
+// }
+
+
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default {
-    dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-    dbName: 'tester_db',
+  dbURL: process.env.ATLAS_URL,
+  dbName: process.env.ATLAS_DB_NAME,
 }
